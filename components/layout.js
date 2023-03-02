f2caa759db27b8b7a0227da886e8ac3865611911
muhsin-life <muhsin.m@life-me.com>
2023-03-02 17:42:17 +0400

    responsive footer

--- components/layout.js
+++ components/layout.js
@@ -1,24 +1,27 @@
 import Navbar from "./navbar"
 import Footer from "./footer"
 
-export default function Layout({ children , data, brands_data}) {
+export default function Layout({ children, data, brands_data }) {
   function searchButtonOnLeave(e) {
     if (!e.target.parentNode.classList.contains("group-search")) {
-      document.getElementsByClassName("lg-screen-searchsuggestion")[0].classList.add("hidden");
+      document.getElementsByClassName("lg-screen-searchsuggestion-lg")[0].classList.add("hidden");
+      document.getElementById("lg-screen-search").classList.remove("p-2", "rounded-t-xl");
+      document.getElementById("lg-screen-search").classList.add("rounded-xl");
+
     }
   }
-  
-    return (
-      <>
-      <div onMouseDown={(e)=>{searchButtonOnLeave(e)}}>
-      <Navbar data={data} brands_data={brands_data}/>
+
+  return (
+    <>
+      <div onMouseDown={(e) => { searchButtonOnLeave(e) }}>
+        <Navbar data={data} brands_data={brands_data} />
         <main>{children}</main>
         <Footer />
       </div>
-      
 
-      </>
-    )
-  }
 
-  
+    </>
+  )
+}
+
+
