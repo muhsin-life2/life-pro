b993b81a247da17603c04c79865ee1c2c21b8003
muhsin-life <muhsin.m@life-me.com>
2023-03-10 17:33:28 +0400

    authentication test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={session } basePath="/">
       <Component {...pageProps} />
     </SessionProvider>
   )
