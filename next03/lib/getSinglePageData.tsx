b957712be74fbcfb7847033faa628b5bc8a1c3f9
muhsin-life2 <128311823+muhsin-life2@users.noreply.github.com>
2023-04-04 11:53:43 +0400

    df

--- next03/lib/getSinglePageData.tsx
+++
@@ -1,9 +0,0 @@
-export default async function getSinglePageData(params) {
-    console.log(params);
-
-    const res = await fetch(`https://prodapp.lifepharmacy.com/api/cms/page/${params}`)
-
-    if (!res.ok) throw new Error('failed to fetch data')
-
-    return res.json()
-}
