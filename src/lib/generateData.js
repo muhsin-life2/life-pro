165e32a325daf75e18cefafca87ffae2cbc62a35
muhsin-life <muhsin.m@life-me.com>
2023-04-04 13:17:48 +0400

    co

--- src/lib/generateData.js
+++
@@ -1,9 +0,0 @@
-export default async function generateData(slug) {
-
-    const res = await fetch(`https://prodapp.lifepharmacy.com/api/cms/page/${slug}`);
-    if (!res.ok) {
-        throw new Error("Unable to fetch Page Data")
-    }
-    return res.json();
-
-}
--- src/pages/api/googleMapLoader.js
+++
@@ -1,8 +0,0 @@
-import { Loader } from "@googlemaps/js-api-loader";
-
-const loader = new Loader({
-    apiKey: 'AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg',
-    version: 'weekly',
-    libraries: ['places'],
-  });
-  export default loader;
