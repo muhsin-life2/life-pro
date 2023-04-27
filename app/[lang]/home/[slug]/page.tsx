bdf41544664211f5494507051f4e176f5869bda9
muhsin-life <muhsin.m@life-me.com>
2023-04-27 12:16:04 +0400

    comiit

--- app/[lang]/home/[slug]/page.tsx
+++ app/[lang]/home/[slug]/page.tsx
@@ -30,14 +30,18 @@ const SinglePageContent = async ({ params }) => {
         const data = await getSinglePageData(params.slug, params.lang)
 
         return (
-            <div className="max-w-[1450px] px-[10px] mx-auto">
-                {data.data.content.map(data => (
-                    <PageStructure data={data} lang={params.lang} >
-                        { /* @ts-expect-error Async Server Component */}
-                        <Products lang={params.lang} slug={data.section_data_object?.slug} type_key={data.section_data_object?.type_key} />
-                    </PageStructure >
-                ))}
-            </div>
+            // <div className="max-w-[1450px] px-[10px] mx-auto">
+            //     {data.data.content.map(data => (
+            //         <PageStructure data={data} lang={params.lang} >
+            //             { /* @ts-expect-error Async Server Component */}
+            //             <Products lang={params.lang} slug={data.section_data_object?.slug} type_key={data.section_data_object?.type_key} />
+            //         </PageStructure >
+            //     ))}
+            // </div>
+            <>
+            <PageStructure data={data.data.content} lang={params.lang} >
+            </PageStructure >
+        </>
         )
     }
 
