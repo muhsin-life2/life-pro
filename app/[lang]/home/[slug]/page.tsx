f36e9766eb62c015c85e0862809059b95a9053fa
muhsin-life <muhsin.m@life-me.com>
2023-05-02 08:18:28 +0400

    commit

--- app/[lang]/home/[slug]/page.tsx
+++ app/[lang]/home/[slug]/page.tsx
@@ -28,8 +28,8 @@ const SinglePageContent = async ({ params }) => {
 
         return (
             <div className="max-w-[1450px] px-[10px] mx-auto">
-                {data.data.content.map(data => (
-                    <PageStructure data={data} lang={params.lang} >
+                {data.data.content.map((data, ind) => (
+                    <PageStructure data={data} lang={params.lang} setLoading={ind=== 0?true:false}>
                         { /* @ts-expect-error Async Server Component */}
                         <Products lang={params.lang} slug={data.section_data_object?.slug} type_key={data.section_data_object?.type_key} />
                     </PageStructure >
